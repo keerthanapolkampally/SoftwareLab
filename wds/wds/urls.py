65bea8ca82c51ff48716599bbf29b6249f510f64
Namrata Ladda <namrataladda@gmail.com>
2018-03-10 16:29:22 +0530

    static files

--- wds/wds/urls.py
+++ wds/wds/urls.py
@@ -22,7 +22,7 @@ from index import views as index_views
 
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
+   # url(r'^admin/', include(admin.site.urls)),
     url(r'^$', index_views.home, name='home'),
     url(r'^login/$', auth_views.login, {'template_name': 'login.html'}, name='login'),
     url(r'^logout/$', auth_views.logout, {'next_page': 'login'}, name='logout'),
